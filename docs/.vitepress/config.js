e90879b2be64ae335c595aec166adc0c8d103d76
RomyLuo034 <luoyq020304@163.com>
2025-07-30 13:11:46 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,14 +1,14 @@
 export default {
   ignoreDeadLinks: true,
   title: 'Romy Luo',
-  description: '个人简介 | 实习作品展示 |',
+  description: '个人简介 | 实习项目展示 |',
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
       { text: '简历', link: '/resume/' },
-      { text: '实习作品', link: '/work/' },
       { text: '财经新闻', link: '/posts/' },
-      { text: '财报分析', link: '/earnings/' }
+      { text: '财报分析', link: '/earnings/' },
+      { text: '实习作品', link: '/work/' }
     ]
   }
 }
